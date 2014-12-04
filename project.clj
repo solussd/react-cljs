c8279a441906cb71c9cad7d4e6ca546018f8f90f
Relevance Pair <pair@thinkrelevance.com>
2014-12-04 14:08:46 -0500

    Use clojars group I own

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject com.facebook/react "0.12.1"
+(defproject solussd/react "0.12.1"
   :description "Facebook's React"
   :url "http://facebook.github.io/react"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo/"
+                                    :sign-releases false}]])
